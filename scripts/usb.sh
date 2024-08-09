f9fdc8a36e33eac78d109158d9ade7f4ca275e24
7s4e <bryant@7s4e.org>
2024-08-09 20:05:20 +0800

    debug

--- scripts/usb.sh
+++ scripts/usb.sh
@@ -14,81 +14,69 @@ CHKOUT_DIR="usb"
 # NAME, EMAIL, TOKEN are also set either as environment variables with 
 # read_data() or as global variables with get_data().
 
+# Function Name: check_mountpoint
+# Description: checks if a USB partition is mounted
+# Parameters: $1: mount point path
+check_mountpoint() {
+    local mnt_pt="$1"
+    if mountpoint --quiet "$mnt_pt"; then
+        findmnt --noheadings --output SOURCE "$mnt_pt"
+    else
+        echo "There is no USB partition mounted at $mnt_pt."
+        return 1
+    fi
+}
+
 # Function Name: clone_repo
 # Description: clone, configure and commit repository with sparse checkout
 # Parameters: none
 clone_repo() {
-
-    # Clone the repository and configure sparse checkout
     git clone --no-checkout "https://$TOKEN@$GIT_HOST/$NAME/$REPO.git"
     cd "$BOOT_MNT_PT/$REPO" || exit
     git config core.sparseCheckout true
     echo "/$CHKOUT_DIR/" > .git/info/sparse-checkout
     git checkout
 
-    # Store credentials and configure git
+    configure_git
+    echo ".gitignore" >> .git/info/sparse-checkout
+
+    git read-tree -mu HEAD
+    git add .gitignore
+    git commit --message "Ignore credentials"
+}
+
+# Function Name: configure_git
+# Description: configure git user and store credentials
+# Parameters: none
+configure_git() {
     git config credential.helper store
     echo "https://$TOKEN@$GIT_HOST" > .git-credentials
     echo ".git-credentials" >> .gitignore
-    echo ".gitignore" >> .git/info/sparse-checkout
-
-    # Update the repository and commit changes
-    git read-tree --merge --update HEAD
-    git add .gitignore
-
-    # Configure git user and commit
     git config user.email "$EMAIL"
     git config user.name "$NAME"
-    git commit --message "Ignore credentials"
 }
 
 # Function Name: configure_grub
 # Description: Link GRUB configuration to updated copy in the C3 repository
 # Parameters: none
 configure_grub() {
-    cd "$BOOT_MNT_PT/grub"
-    ln --symbolic "../$REPO/$CHKOUT_DIR/grub.cfg" grub.cfg
-    echo "Setting GRUB configuration..."
+    ln --symbolic \
+        "$BOOT_MNT_PT/$REPO/$CHKOUT_DIR/grub.cfg" "$BOOT_MNT_PT/grub/grub.cfg"
 }
 
 # Function Name: confirm_mounting
 # Description: Confirm USB partitions are correctly mounted
 # Parameters: none
 confirm_mounting() {
-
-    # Variables for mounting status, mounted devices, and confirmation prompt
-    local has_esp has_boot
     local esp_device boot_device
+    esp_device=$(check_mountpoint "$ESP_MNT_PT") || exit
+    boot_device=$(check_mountpoint "$BOOT_MNT_PT") || exit
+
     local prompt
-
-    # Check for USB partition mounted to /mnt/esp
-    if mountpoint --quiet "$ESP_MNT_PT"; then
-        has_esp=true
-        esp_device=$(findmnt --noheadings --output SOURCE "$ESP_MNT_PT")
-    else
-        has_esp=false
-        echo "There is no USB partition mounted at $ESP_MNT_PT."
-    fi
-
-    # Check for USB partition mounted to /boot/esp
-    if mountpoint --quiet "$BOOT_MNT_PT"; then
-        has_boot=true
-        boot_device=$(findmnt --noheadings --output SOURCE "$BOOT_MNT_PT")
-    else
-        has_boot=false
-        echo "There is no USB partition mounted at $BOOT_MNT_PT."
-    fi
-
-    # Prompt user for confirmation, and put instructions as appropriate
-    if [[ $has_esp == false || $has_boot == false ]]; then
-        echo "Mount the USB before re-running this script."
+    prompt="Is $esp_device at $ESP_MNT_PT and $boot_device at $BOOT_MNT_PT correct? (y/n) "
+    if ! query_boolean "$prompt" false; then
+        echo "Correct the USB partition mounts before re-running this script."
         exit
-    else
-        prompt="Is $esp_device at $ESP_MNT_PT and $boot_device at $BOOT_MNT_PT correct? (y/n) "
-        if !(query_boolean "$prompt" false); then
-            echo "Correct the USB partition mounts before re-running this script."
-            exit
-        fi
     fi
 }
 
@@ -106,7 +94,8 @@ get_data() {
 # Description: Install GRUB on USB drive
 # Parameters: none
 install_grub() {
-    local device=$(df "$BOOT_MNT_PT/$REPO" | awk "NR==2 {print $1}")
+    local device
+    device=$(df "$BOOT_MNT_PT/$REPO" | awk "NR==2 {print $1}")
     grub-install --target=x86_64-efi --efi-directory="$ESP_MNT_PT" \
         --boot-directory="$BOOT_MNT_PT" --removable --recheck --no-floppy \
         "$device"
@@ -116,9 +105,9 @@ install_grub() {
 # Description: Read Github configuration data from file, if able
 # Parameters: none
 read_data() {
-    local data=$(jq --raw-output ".git" $DATA_FILE)
-    set_vars_from_obj $data
-    echo "Reading Github configuration data..."
+    local data
+    data=$(jq --raw-output ".git" $DATA_FILE)
+    set_vars_from_obj "$data"
 }
 
 # Function Name: update_repo
@@ -126,13 +115,14 @@ read_data() {
 # Parameters: none
 update_repo() {
     git fetch origin
-    git read-tree --merge --update HEAD
+    git read-tree -mu HEAD
     git rebase origin/main
 }
 
-# MAIN
+# MAIN SCRIPT EXECUTION
 elevate_privileges
 confirm_mounting
+
 if [[ -d "$BOOT_MNT_PT/$REPO" ]]; then
     cd "$BOOT_MNT_PT/$REPO" || exit
     update_repo
@@ -142,6 +132,7 @@ else
     else
         get_data
     fi
+    cd "$BOOT_MNT_PT" || exit
     clone_repo
     install_grub
     configure_grub
