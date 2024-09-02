c95e71f9ad879c1cd3ad9fb1f40d28a3d6ccd415
7s4e <bryant@7s4e.org>
2024-09-03 06:20:12 +0800

    start mk boot dsk

--- scripts/README.md
+++ scripts/README.md
@@ -3,5 +3,6 @@
 | --- | --- |
 | [functions](functions.py) | module |
 | [get_disk](get_disk.py) | module<br>script |
+| [make_boot_disk](make_boot_disk.py) | script |
 | [partition_disk](partition_disk.py) | script |
 | [table](table.py) | class |
---
+++ scripts/make_boot_disk.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+from get_disk import get_disk
+
+
+def make_boot_disk(disk: str | None = None) -> None:
+    if disk is None:
+        disk = get_disk()
+    print(f"MBD>mbd-disk: {disk}")
+
+
+def main() -> None:
+    make_boot_disk()
+
+
+if __name__ == "__main__":
+    main()
+
+# print(f"MBD>mbd-disk: {disk}")
+
+# ## Generic Preparation
+# * `$ lsblk` to identify storage devices
+# * `# umount /dev/sdXn` to unmount
+# * `# badblocks -sv /dev/sdX` to check health status
+#     * `-sv` read-only test
+#     * `-nsv` non-destructive read-write test
+#     * `-wsv` destructive read-write test
+# * `# parted /dev/sdX`
+#     * `mklabel gpt` to create partition table
+#     * `mkpart primary 0% 100%` to create partition
+#     * `set 1 msftdata on` for Windows data
+# * `# mkfs.exfat -n "My Label" /dev/sdXn`
