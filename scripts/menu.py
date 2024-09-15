473f6b55da02fe2fe248459aef5d741a4cce8ce9
7s4e <bryant@7s4e.org>
2024-09-16 05:58:16 +0800

    w set prompt

--- scripts/menu.py
+++ scripts/menu.py
@@ -6,6 +6,7 @@ from typing import Any
 from blessed import Terminal
 
 # Local module import
+from console import Console_Prompt
 from table import Table
 
 class Menu:
@@ -18,8 +19,17 @@ class Menu:
                                     table_data=[{"OPTION": option} 
                                                  for option in options]))
         self._count = self._options.count_records()
-        self._prompt = f"Enter number (1-{self._count}) for selection: "
+        self.set_prompt()
     
+    def get_selection(self, key: str = "OPTION") -> Any:
+        return self._selection[key.upper()]
+
+    def run(self, terminal: Terminal) -> None:
+        # self._options.put_table(display_width=len(self._prompt), is_menu=True)
+        self._options.put_table(terminal=terminal, is_menu=True)
+        index = self._prompt_selection() - 1
+        self._selection = self._options.get_record(index)
+
     def _prompt_selection(self) -> int:
         while True:
             try:
@@ -29,15 +39,12 @@ class Menu:
                 print(f"Enter a number between 1 and {self._count}.")
             except ValueError:
                 print("Enter a valid number.")
-
-    def get_selection(self, key: str = "OPTION") -> Any:
-        return self._selection[key.upper()]
-
-    def run(self, terminal: Terminal) -> None:
-        # self._options.put_table(display_width=len(self._prompt), is_menu=True)
-        self._options.put_table(terminal=terminal, is_menu=True)
-        index = self._prompt_selection() - 1
-        self._selection = self._options.get_record(index)
+    
+    def set_prompt(self, prompt: str | None = None) -> None:
+        default = f"Enter number (1-{self._count}) for selection:"
+        self._prompt = (Console_Prompt(default) 
+                        if prompt is None 
+                        else Console_Prompt(prompt))
 
     @staticmethod
     def query_yes_no(prompt: str) -> bool:
