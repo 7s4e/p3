e51dcb925bc24803e84fbcac3ec9ed9afc3ac700
7s4e <131584596+7s4e@users.noreply.github.com>
2024-12-20 13:44:41 +0800

    restructure rowContent

--- scripts/console/test_console_table_methods.py
+++ scripts/console/test_console_table_methods.py
@@ -10,14 +10,33 @@ ROW_ENDS = {"top": ("<blue>╔</blue>", "<blue>╗</blue>", ""),
             "txt": ("<blue>║</blue>", "<blue>║</blue>", " ")}
 
 ROW_CONTENT = {
-    "top": {"raw": "═", "prc": "<blue>══════════════</blue>"}, 
-    "inr": {"raw": "─", "prc": "<blue>──────────────</blue>"}, 
-    "btm": {"raw": "═", "prc": "<blue>══════════════</blue>"}, 
-    "ttl": {"raw": "Data Title", "prc": ["<reverse> Data Title </reverse>"]}, 
-    "hdg": {"raw": {"COL A": "COL A", "COL B": "COL B"}, 
-            "prc": ["<underline>COL A</underline>", 
-                    "<underline>COL B</underline>"]}, 
-    "rec": {"raw": {"COL A": "abc", "COL B": "123"}, "prc": ["abc  ", "  123"]}
+    "raw": {
+        "top": "═", 
+        "inr": "─", 
+        "btm": "═", 
+        "ttl": "Data Title", 
+        "hdg": {"COL A": "COL A", "COL B": "COL B"}, 
+        "rec": {"COL A": "abc", "COL B": "123"}
+    }, 
+    "prc": {
+        "top": "<blue>══════════════</blue>", 
+        "inr": "<blue>──────────────</blue>", 
+        "btm": "<blue>══════════════</blue>", 
+        "ttl": ["<reverse> Data Title </reverse>"], 
+        "hdg": ["<underline>COL A</underline>", 
+                "<underline>COL B</underline>"], 
+        "rec": ["abc  ", "  123"]
+    }, 
+    "fin": {
+        "top": "<blue>╔</blue><blue>══════════════</blue><blue>╗</blue>\n", 
+        "inr": "<blue>╟</blue><blue>──────────────</blue><blue>╢</blue>\n", 
+        "btm": "<blue>╚</blue><blue>══════════════</blue><blue>╝</blue>\n", 
+        "ttl": "<blue>║</blue> <reverse> Data " + 
+                "Title </reverse> <blue>║</blue>\n", 
+        "hdg": "<blue>║</blue> <underline>COL A</underline>  " + 
+                "<underline>COL B</underline> <blue>║</blue>\n", 
+        "rec": "<blue>║</blue> abc      123 <blue>║</blue>\n"
+    }
 }
 
 
@@ -32,9 +51,9 @@ def console_mock(mocker):
 @pytest.fixture
 def data_mock(mocker):
     mock = mocker.Mock(spec=Table)
-    mock.get_title.return_value = ROW_CONTENT["ttl"]["raw"]
-    mock.get_headings.return_value = ROW_CONTENT["hdg"]["raw"]
-    mock.get_record.return_value = ROW_CONTENT["rec"]["raw"]
+    mock.get_title.return_value = ROW_CONTENT["raw"]["ttl"]
+    mock.get_headings.return_value = ROW_CONTENT["raw"]["hdg"]
+    mock.get_record.return_value = ROW_CONTENT["raw"]["rec"]
     mock.get_rjust_columns.return_value = {"COL B"}
     mock.get_column_widths.return_value = {"COL A": 5, "COL B": 5}
     mock.get_table_width.return_value = 12
@@ -52,77 +71,76 @@ def con_tbl_inst(data_mock, console_mock):
 
 # Test drawRow
 @pytest.mark.parametrize(
-    "row_type, ends, raw, processed, index, is_line_type, exp_out",
+    "row_type, ends_in, raw_in, processed_in, record_idx, is_line_type, exp_out",
     [
-        (
+        (   # Test case 1: Top border line
             "top", 
             ROW_ENDS["top"], 
-            ROW_CONTENT["top"]["raw"], 
-            ROW_CONTENT["top"]["prc"], 
+            ROW_CONTENT["raw"]["top"], 
+            ROW_CONTENT["prc"]["top"], 
             None, 
             True, 
-            "<blue>╔</blue><blue>══════════════</blue><blue>╗</blue>\n"
+            ROW_CONTENT["fin"]["top"]
         ), 
-        (
+        (   # Test case 2: Inner border line
             "inner", 
             ROW_ENDS["inr"], 
-            ROW_CONTENT["inr"]["raw"], 
-            ROW_CONTENT["inr"]["prc"], 
+            ROW_CONTENT["raw"]["inr"], 
+            ROW_CONTENT["prc"]["inr"], 
             None, 
             True, 
-            "<blue>╟</blue><blue>──────────────</blue><blue>╢</blue>\n"
+            ROW_CONTENT["fin"]["inr"]
         ), 
-        (
+        (   # Test case 3: Bottom border line
             "bottom", 
             ROW_ENDS["btm"], 
-            ROW_CONTENT["btm"]["raw"], 
-            ROW_CONTENT["btm"]["prc"], 
+            ROW_CONTENT["raw"]["btm"], 
+            ROW_CONTENT["prc"]["btm"], 
             None, 
             True, 
-            "<blue>╚</blue><blue>══════════════</blue><blue>╝</blue>\n"
+            ROW_CONTENT["fin"]["btm"]
         ), 
-        (
+        (   # Test case 4: Title row
             "title", 
             ROW_ENDS["txt"], 
-            ROW_CONTENT["ttl"]["raw"], 
-            ROW_CONTENT["ttl"]["prc"], 
+            ROW_CONTENT["raw"]["ttl"], 
+            ROW_CONTENT["prc"]["ttl"], 
             None, 
-            False,
-            "<blue>║</blue> <reverse> Data Title </reverse> <blue>║</blue>\n"
+            False, 
+            ROW_CONTENT["fin"]["ttl"]
         ), 
-        (
+        (   # Test case 5: Headings row
             "headings", 
             ROW_ENDS["txt"], 
-            ROW_CONTENT["hdg"]["raw"], 
-            ROW_CONTENT["hdg"]["prc"], 
+            ROW_CONTENT["raw"]["hdg"], 
+            ROW_CONTENT["prc"]["hdg"], 
             None, 
-            False,
-            "<blue>║</blue> <underline>COL A</underline>  " + 
-            "<underline>COL B</underline> <blue>║</blue>\n"
+            False, 
+            ROW_CONTENT["fin"]["hdg"]
         ), 
-        (
+        (   # Test case 6: Record rows
             "record", 
             ROW_ENDS["txt"], 
-            ROW_CONTENT["rec"]["raw"], 
-            ROW_CONTENT["rec"]["prc"], 
+            ROW_CONTENT["raw"]["rec"], 
+            ROW_CONTENT["prc"]["rec"], 
             0, 
-            False,
-            "<blue>║</blue> abc      123 <blue>║</blue>\n"
+            False, 
+            ROW_CONTENT["prc"]["rec"]
         )
     ]
 )
-def test_draw_row(mocker, con_tbl_inst, data_mock, row_type, ends, raw, 
-                  processed, index, is_line_type, exp_out, capfd):
+def test_draw_row(mocker, con_tbl_inst, data_mock, row_type, ends_in, raw_in, 
+                  processed_in, record_idx, is_line_type, exp_out, capfd):
     # Setup attributes
     con_tbl_inst._margin_size = 0
     con_tbl_inst._column_widths = data_mock.get_column_widths.return_value
     con_tbl_inst._table_width = data_mock.get_table_width.return_value
 
     # Setup methods
-    mocker.patch.object(con_tbl_inst, "_get_row_ends", return_value=ends)
-    mocker.patch.object(con_tbl_inst, "_get_row_content", return_value=raw)
+    mocker.patch.object(con_tbl_inst, "_get_row_ends", return_value=ends_in)
+    mocker.patch.object(con_tbl_inst, "_get_row_content", return_value=raw_in)
     mocker.patch.object(con_tbl_inst, "_process_row_content", 
-                        return_value=processed)
+                        return_value=processed_in)
     
     # Setup method argument
     rjust_col = (data_mock.get_rjust_columns.return_value 
@@ -137,9 +155,10 @@ def test_draw_row(mocker, con_tbl_inst, data_mock, row_type, ends, raw,
         data_mock.get_rjust_columns.assert_called_once()
     con_tbl_inst._get_row_ends.assert_called_once_with(row_type, is_line_type)
     if row_type in ["title", "headings", "record"]:
-        con_tbl_inst._get_row_content.assert_called_once_with(row_type, index)
+        con_tbl_inst._get_row_content.assert_called_once_with(row_type, 
+                                                              record_idx)
         con_tbl_inst._process_row_content.assert_called_once_with(row_type, 
-                                                                  raw, 
+                                                                  raw_in, 
                                                                   rjust_col)
     
     # Verify output    
@@ -152,16 +171,16 @@ def test_draw_row(mocker, con_tbl_inst, data_mock, row_type, ends, raw,
 
 # Test getRowContent
 @pytest.mark.parametrize(
-    "row_type, index", 
+    "row_type, record_idx", 
     [
         ("title", None),     # Test case 1: Title
         ("headings", None),  # Test case 2: Headings
         ("record", 0)        # Test case 3: Record
     ]
 )
-def test_get_row_content(con_tbl_inst, data_mock, row_type, index):
+def test_get_row_content(con_tbl_inst, data_mock, row_type, record_idx):
     # Execute
-    result = con_tbl_inst._get_row_content(row_type, index)
+    result = con_tbl_inst._get_row_content(row_type, record_idx)
 
     # Verify
     match row_type:
@@ -172,7 +191,7 @@ def test_get_row_content(con_tbl_inst, data_mock, row_type, index):
             con_tbl_inst._data.get_headings.assert_called_once()
             assert result == data_mock.get_headings.return_value
         case "record":
-            con_tbl_inst._data.get_record.assert_called_once_with(index)
+            con_tbl_inst._data.get_record.assert_called_once_with(record_idx)
             assert result == data_mock.get_record.return_value
 
 
@@ -205,18 +224,22 @@ def test_get_row_ends(con_tbl_inst, row_type, is_line_type, expected):
 @pytest.mark.parametrize(
     "row_type, expected", 
     [
-        # Test case 1: Title line
-        ("title", ROW_CONTENT["ttl"]["prc"]), 
+        # Test case 1: Title row
+        ("title", ROW_CONTENT["prc"]["ttl"]), 
 
-        # Test case 2: Headings line
-        ("headings", ROW_CONTENT["hdg"]["prc"]), 
+        # Test case 2: Headings row
+        ("headings", ROW_CONTENT["prc"]["hdg"]), 
 
-        # Test case 3: Record line
-        ("record", ROW_CONTENT["rec"]["prc"])
+        # Test case 3: Record row
+        ("record", ROW_CONTENT["prc"]["rec"])
     ]
 )
-def test_process_row_content(data_mock, con_tbl_inst, row_type, expected):
-    # Setup
+def test_process_row_content(con_tbl_inst, data_mock, row_type, expected):
+    # Setup attributes
+    con_tbl_inst._column_widths = data_mock.get_column_widths.return_value
+    con_tbl_inst._table_width = data_mock.get_table_width.return_value
+            
+    # Setup arguments
     match row_type:
         case "title":
             content = data_mock.get_title.return_value
@@ -225,8 +248,6 @@ def test_process_row_content(data_mock, con_tbl_inst, row_type, expected):
         case "record":
             content = data_mock.get_record.return_value
     rjust_col = data_mock.get_rjust_columns.return_value
-    con_tbl_inst._column_widths = data_mock.get_column_widths.return_value
-    con_tbl_inst._table_width = data_mock.get_table_width.return_value
     
     # Execute
     result = con_tbl_inst._process_row_content(row_type, content, rjust_col)
