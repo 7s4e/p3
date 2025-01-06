314afc7416f19e6e7d3d209320d2fd1d4806bf94
7s4e <131584596+7s4e@users.noreply.github.com>
2025-01-06 14:19:31 +0800

    update docstring

--- pyScripts/src/modules/__init__.py
+++ pyScripts/src/modules/__init__.py
@@ -6,15 +6,18 @@ listing the modules and classes to be exposed when using
 `from modules import *`.
 
 Modules:
-    - commands: Provides various commands for the package.
-    - utilities: Contains utility functions and helpers.
+    commands: Provides various commands for the package.
+    utilities: Contains utility functions and helpers.
 
 Classes:
-    - Console: Handles console-related operations.
-    - ConsolePrompt: Facilitates console-based user prompts.
-    - ConsoleTable: Manages console-based table rendering.
-    - Menu: Supports interactive menu creation and management.
-    - Table: Implements table-related functionalities.
+    Console: Handles console-related operations.
+    ConsoleAnyKeyPrompt: Prompts for any key from user.
+    ConsoleBoolPrompt: Prompts for yes/no response from user.
+    ConsoleFreeFormPrompt: Prompts for any response from user.
+    ConsoleIntegerPrompt: Prompts for validated integer from user.
+    ConsoleTable: Manages console-based table rendering.
+    Menu: Supports interactive menu creation and management.
+    Table: Implements table-related functionalities.
 
 __all__:
     Defines symbols to be exported when using `from modules import *`.
